0efb7e92c3ddd9f4f1473da415927fe7ac129c96
Daniel Miller <danm1113@gmail.com>
2022-02-20 20:55:44 -0800

    Perfect Numbers solution

--- perfect-numbers/perfect-numbers.rkt
+++ perfect-numbers/perfect-numbers.rkt
@@ -2,4 +2,28 @@
 
 (provide classify)
 
+(define (factor? num f) (equal? (modulo num f) 0))
+(define (factors num)
+  (stream-fold
+    (lambda (acc i) (let
+      ([other_factor (/ num i)])
+      (cond
+        [(or
+          (not(factor? num i))
+          (equal? i num)
+          (not (equal? (member i acc) #f)))
+            acc]
+        [(or
+          (equal? i 1)
+          (equal? i other_factor))
+            (cons i acc)]
+        [else (cons (/ num i) (cons i acc))])))
+    '()
+    (in-range 1 (ceiling (sqrt num)))))
 
+(define (classify x) (let
+  ([total (apply + (factors x))])
+  (cond
+    [(equal? total x) 'perfect]
+    [(< total x) 'deficient]
+    [(> total x) 'abundant])))

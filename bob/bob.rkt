e88c65de28a31e163ae6483e4d765694bfd0aea5
Daniel Miller <dmiller@farmersbusinessnetwork.com>
2022-05-24 11:50:57 -0700

    Move to match syntax

--- bob/bob.rkt
+++ bob/bob.rkt
@@ -2,24 +2,22 @@
 
 (define
     [response-for raw-phrase]
-        (let*
-            ([phrase (string-trim raw-phrase)]
-            [is-question? (string-suffix? phrase "?")]
-            [is-shouting?
-                (for/fold
-                    ([has-capital #f]
-                    [has-no-lowercase #t]
-                    #:result (and has-capital has-no-lowercase))
-                 ([c (string->list phrase)])
-        (values
-            (or has-capital (char-upper-case? c))
-            (and has-no-lowercase (not (char-lower-case? c))))) ])
-        (cond
-            [(equal? (non-empty-string? phrase) #f) "Fine. Be that way!"]
-            [is-shouting? (if is-question?
-                "Calm down, I know what I'm doing!"
-                "Whoa, chill out!")]
-            [is-question? "Sure."]
-            [else "Whatever."])))
+        (let ([phrase (string-trim raw-phrase)])
+        (match (list
+            (false? (non-empty-string? phrase))
+            (string-suffix? phrase "?")
+            (for/fold
+                ([has-capital #f]
+                [has-no-lowercase #t]
+                #:result (and has-capital has-no-lowercase))
+                ([c (string->list phrase)])
+            (values
+                (or has-capital (char-upper-case? c))
+                (and has-no-lowercase (not (char-lower-case? c))))))
+         ((list #t _ _) "Fine. Be that way!")
+         ((list _ #t #t) "Calm down, I know what I'm doing!")
+         ((list _ #f #t) "Whoa, chill out!")
+         ((list _ #t #f) "Sure.")
+         (_ "Whatever."))))
 
 (provide response-for)

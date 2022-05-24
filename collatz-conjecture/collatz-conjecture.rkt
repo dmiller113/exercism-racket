b7ad23c5b6fed458528913bc54f77e190fb6e8d4
Daniel Miller <dmiller@farmersbusinessnetwork.com>
2022-05-24 13:36:19 -0700

    Solve Collatz Conjecture

--- collatz-conjecture/collatz-conjecture.rkt
+++ collatz-conjecture/collatz-conjecture.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
+(define (collatz-number num) (cond
+    [(even? num) (/ num 2)]
+    [else (+ (* num 3) 1)]))
+
+(define (collatz num [i 0]) (cond
+    [(= num 0) (error "0")]
+    [(< num 0) (error "Nonpositive number")]
+    [(inexact? num) (error "inexact number")]
+    [(= num 1) i]
+    [else (collatz (collatz-number num) (add1 i))]))
+
 (provide collatz)
-
-  
-
